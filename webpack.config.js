c43125da99423998007d99e2bd4d4a095613168e
Kyle Graas <kgraas09@gmail.com>
2020-09-23 19:23:07 -0600

    Reconfigured webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -5,22 +5,22 @@ module.exports = {
   entry: './client/src/index.js',
   output: {
     path: path.join(__dirname, './client/dist'),
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
+        test: /\.jsx?/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './client/src/index.html'
-    })
-  ]
-}
+      template: './client/src/index.html',
+    }),
+  ],
+};
