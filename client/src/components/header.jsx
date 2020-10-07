322f3a5fe17cd289c2d9e9ae0e6c4b93d28019c4
Kyle Graas <kgraas09@gmail.com>
2020-10-07 14:36:41 -0600

    Edited styling

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
 import Cart from './Cart';
 import Searchbar from './Searchbar';
 
 const Header = ({userSessionData, productId, productList}) => (
   <div id="header-style-kg">
     <div className="box-style-kg">
-      <h2>t h r 3 d s</h2>
+      <h2 className="header-logo-kg">t h r 3 d s</h2>
       <p className="slogan-style-kg">what's fresher than fresh? hygenic.</p>
     </div>
     <div>
