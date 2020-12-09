8ce73dd915b2dfb1309980d603b31ed74603f3e6
Kyle Graas <kgraas09@gmail.com>
2020-12-08 17:15:33 -0700

    Changed "http" to "https" in axios requests so Heroku can send the data once deployed.  Hoping this works.

--- client/src/API/GetRatings.js
+++ client/src/API/GetRatings.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-unresolved
 import axios from 'axios';
 
-const getRatings = (productId) => axios.get('http://52.26.193.201:3000/reviews/' + productId + '/meta')
+const getRatings = (productId) => axios.get('https://52.26.193.201:3000/reviews/' + productId + '/meta')
   .then((res) => res.data)
   .catch((err) => { throw err; });
 
--- client/src/API/PostCart.js
+++ client/src/API/PostCart.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-unresolved
 import axios from 'axios';
 
-const addToCart = (userSessionData) => axios.post('http://52.26.193.201:3000/cart/', userSessionData)
+const addToCart = (userSessionData) => axios.post('https://52.26.193.201:3000/cart/', userSessionData)
   .then((res) => res.data)
   .catch((err) => { throw err; });
 
